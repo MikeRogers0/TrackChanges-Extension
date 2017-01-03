12b6e0df92aaca588117cda363a6f700c960674d
Mike Rogers <me@mikerogers.io>
2017-01-03 20:19:44 +1300

    Adding in build folders and removing tabs to bg

--- src/background/scripts/background.js
+++ src/background/scripts/background.js
@@ -2,6 +2,7 @@
 
 // Saves the tabs MHTML on page load, clear the store on tab close.
 var tabsMHTML = {};
+var popupOpen = false;
 
 // Cache the HTML to memory.
 function cacheMHTML(tabId){
@@ -24,4 +25,25 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
 // Clear up the memory on tab close.
 chrome.tabs.onRemoved.addListener(function(tabId, removeInfo){
   delete tabsMHTML[tabId];
+});
+
+// If the user clicks the icon - toggle the popup, I might be able to remove this.
+chrome.browserAction.onClicked.addListener(function(tab) {
+  // If popup is open, close it.
+  if (popupOpen){
+    popupOpen = false;
+
+    chrome.browserAction.setPopup({
+      tabId: tab.id,
+      popup: ""
+    });
+
+    return;
+  }
+
+  // Otherwise open the popup and let the popup.js do the rest.
+  chrome.browserAction.setPopup({
+    tabId: tab.id,
+    popup: "src/popup.html"
+  });
 });
--- src/background/scripts/popup.js
+++ src/background/scripts/popup.js
@@ -6,6 +6,4 @@ let jsdiff = from 'diff';
 // https://github.com/zsxsoft/mhtml-parser
 let parser = from 'mhtml-parser';
 
-// The user clicked the icon, open the popup and compare the diff.
-chrome.browserAction.onClicked.addListener(function(tab) {
-});
+background = chrome.extension.getBackgroundPage();
