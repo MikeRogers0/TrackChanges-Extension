9b740488d6937be2020a0c206ce3f23a59508476
Mike Rogers <me@mikerogers.io>
2017-01-03 22:01:28 +1300

    Messing around with how to parse the data

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,8 @@ import babel from 'rollup-plugin-babel';
 
 export default {
   entry: 'src/content_scripts/js/scripts.js',
-  plugins: [ babel() ],
-  format: 'umd'
+  plugins: [ 
+    babel() 
+  ],
+  format: 'iife'
 };
--- src/browser_action/popup.js
+++ src/browser_action/popup.js
@@ -4,7 +4,7 @@
 import JsDiff from 'diff';
 
 // https://github.com/zsxsoft/mhtml-parser
-//import parseByString from 'mhtml-parser';
+import parseByString from 'mhtml-parser';
 
 var background = chrome.extension.getBackgroundPage();
 var tab = null;
@@ -32,7 +32,7 @@ function runTheDiff(){
 function loadOriginalmHTMLData(){
   var reader = new window.FileReader();
   reader.onload = function() {
-    original_html_data = reader.result;
+    original_mhtmlData = reader.result;
     buildDiff();
   };
   reader.readAsBinaryString(background.tabsMHTML[tab.id]);
@@ -41,11 +41,23 @@ function loadOriginalmHTMLData(){
 function buildDiff(){
   // background.tabsMHTML[tab.id] vs  new_mhtmlData
   
-  var diff = JsDiff.diffLines(original_mhtmlData, new_mhtmlData);
+  window.data = parser.parse(original_mhtmlData);
+  console.log(window.data);
 
-  diffElm.innerHTML = '<code>';
-  diffElm.innerHTML += diff;
-  diffElm.innerHTML += '</code>';
+  // These will be parsed into files I think, only looking at HTML/CSS/JS.
+  //var diff = JsDiff.diffLines(original_mhtmlData, new_mhtmlData);
+
+  //window.diff = diff;
+
+  //[diff[0], diff[1], diff[2], diff[3], diff[4]].forEach(function(part){
+    //// green for additions, red for deletions
+    //// grey for common parts
+    //var color = part.added ? 'green' : part.removed ? 'red' : 'grey';
+    //var span = document.createElement('div');
+    //span.style.color = color;
+    //span.appendChild(document.createTextNode(part.value));
+    //diffElm.appendChild(span);
+  //});
 
   //diffElm.innerHTML = '<p><a href="' + window.URL.createObjectURL(background.tabsMHTML[tab.id]) + '">Original</a></p>';
   //diffElm.innerHTML += '<p><a href="' + window.URL.createObjectURL(new_mhtmlData) + '">Updated</a></p>';
