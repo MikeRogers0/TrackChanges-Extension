448e5f36638782492e1b11cd957e0f306bccfbdf
Mike Rogers <me@mikerogers.io>
2017-08-13 18:04:29 +0100

    Ditching panel view

--- src/devtools/panel-sidebar.js
+++ src/devtools/panel-sidebar.js
@@ -1,1 +1,22 @@
 // List the created snapshots.
+import { ChromeFiles } from '../shared/chrome-files';
+
+var fileList = document.querySelector(".file-list");
+
+function renderSnapshotListItem(result){
+  var listItem = document.querySelector('[data-template="fileListItem"]').innerHTML;
+  listItem = listItem.replace(/#{id}/g, result.name);
+  listItem = listItem.replace(/#{date}/g, new Date(parseInt(result.name)).toLocaleDateString());
+  return listItem;
+}
+
+ChromeFiles().listFoldersInRootDirectory(function(results){
+  fileList.innerHTML = '';
+  for(var i in results){
+    var result = results[i];
+    if(result.isDirectory){
+      debugger;
+      fileList.innerHTML += renderSnapshotListItem(result);
+    }
+  }
+});
--- src/devtools/panel-view.js
+++ src/devtools/panel-view.js
@@ -1,1 +1,18 @@
 // List the created snapshot.
+
+function getJsonFromUrl() {
+  var query = location.search.substr(1);
+  var result = {};
+  query.split("&").forEach(function(part) {
+    var item = part.split("=");
+    result[item[0]] = decodeURIComponent(item[1]);
+  });
+  return result;
+}
+
+var snapshotId = getJsonFromUrl()["id"];
+if( snapshotId.length > 0 ){
+  // Hide the show snapshot
+  // Load the snapshot.
+}
+
