7931cdf8edb1db255b3d89129496ec02211dcec7
Mike Rogers <me@mikerogers.io>
2017-11-20 16:40:48 +0000

    Catching a few edge cases from crazy HTML libiaries

--- src/shared/html-cleaner.js
+++ src/shared/html-cleaner.js
@@ -1,14 +1,60 @@
 export function HTMLCleaner(html, options) {
   var dom = null;
 
-  function ignoreCommonlyInjectedElements(){
-    dom.querySelectorAll(options.ignoreCommonlyInjectedSelectors.join(', ')).forEach(function(element) {
-      element.remove();
-    });
+  // TODO = Decide on the elements to include for these.
+  // Bootstrap elements 3/4
+  // Foundation Elements
+  // https://flyntwp.com/
+
+  var defaults = {
+    commonlyInjectedSelectors: [
+      'script',
+      'head > link',
+      'head > style',
+      'iframe',
+      // Intercom
+      '#intercom-stylesheet, #intercom-container',
+      // Drift
+      '#drift-widget-container',
+      // Turbolinks
+      '#nprogress',
+      // Google Maps
+      '.maps-wrapper',
+      // Nickelled
+      '.nickelled__branding, [data-nickelled-styling], [data-nickelled-component]',
+      // Stripe
+      '.__PrivateStripeElement',
+      // Zendesk,
+      '#contact-widget-launcher, #contact-widget'
+    ],
+    ignoreHtmlSelectors: [
+      '.client-card > a',
+      'svg > *'
+    ],
+    ignoreInlineStyles: {
+      '*': 'transform',
+      '.modal': 'display',
+      'h1.page-title': 'opacity',
+    },
+    ignoreAttributes: {
+      '.lazyloaded, .lazyload, .img-responsive, .lazy-load .lazy-loaded .lazy': 'src data-src data-srcset sizes width height data-original',
+      '.client-card img': 'alt width height',
+      '[data-scene]': 'data-arrival-index data-departure-index',
+      '.is_stuck, [class*="360View"], [class*="360View"], [class*="Animated"], [class*="animated"]' : 'style'
+    },
+    ignoreClassNames: {
+      '*': 'front-visible animation-ended lazyloaded lazyload lazy-load lazy-loaded nprogress-busy is-scrolling',
+      '.carousel-inner .item': 'active',
+      '.animate': 'animate-in',
+      '.modal': 'in',
+      '.arrow-wrapper': 'show-label',
+      '.cookie-hint': 'active'
+    }
   }
 
-  function ignoreHtmlSelector(){
-    dom.querySelectorAll(options.ignoreHtmlSelectors.join(', ')).forEach(function(element) {
+
+  function ignoreHtmlSelectors(selector){
+    dom.querySelectorAll(selector).forEach(function(element) {
       element.remove();
     });
   }
@@ -16,8 +62,8 @@ export function HTMLCleaner(html, options) {
   // Ignore when an element has styles like:
   // <div style="matrix: 1010; display: none; transform: none;" />
   function ignoreInlineStyles(){
-    Object.keys( options.ignoreInlineStyles ).forEach(function(selector, index){
-      var styles = options.ignoreInlineStyles[selector];
+    Object.keys( defaults.ignoreInlineStyles ).forEach(function(selector, index){
+      var styles = defaults.ignoreInlineStyles[selector];
       dom.querySelectorAll(selector).forEach(function(element) {
         styles.split(' ').forEach(function(style) {
           element.style.removeProperty(style.trim());
@@ -32,8 +78,8 @@ export function HTMLCleaner(html, options) {
 
   // If an element has attributes like 'data-src', we can safely ignore those also.
   function ignoreAttributes(){
-    Object.keys( options.ignoreAttributes ).forEach(function(selector, index){
-      var attributes = options.ignoreAttributes[selector];
+    Object.keys( defaults.ignoreAttributes ).forEach(function(selector, index){
+      var attributes = defaults.ignoreAttributes[selector];
       dom.querySelectorAll(selector).forEach(function(element) {
         attributes.split(' ').forEach(function(attribute) {
           element.removeAttribute(attribute.trim());
@@ -44,8 +90,8 @@ export function HTMLCleaner(html, options) {
 
   // Remoce classnames like in/animate-in
   function ignoreClassNames(){
-    Object.keys( options.ignoreClassNames ).forEach(function(selector, index){
-      var classNames = options.ignoreClassNames[selector];
+    Object.keys( defaults.ignoreClassNames ).forEach(function(selector, index){
+      var classNames = defaults.ignoreClassNames[selector];
       dom.querySelectorAll(selector).forEach(function(element) {
         classNames.split(' ').forEach(function(className) {
           element.classList.remove(className.trim());
@@ -59,11 +105,11 @@ export function HTMLCleaner(html, options) {
     dom = parser.parseFromString(html, "text/html");
 
     if(options.ignoreCommonlyInjectedElements){
-      ignoreCommonlyInjectedElements();
+      ignoreHtmlSelectors(defaults.commonlyInjectedSelectors.join(', '));
     }
 
     if(options.ignoreDynamicElements){
-      ignoreHtmlSelector();
+      ignoreHtmlSelectors(defaults.ignoreHtmlSelectors.join(', '));
       ignoreAttributes();
       ignoreInlineStyles();
       ignoreClassNames();
--- src/shared/track-changes-options.js
+++ src/shared/track-changes-options.js
@@ -1,36 +1,5 @@
 export function TrackChangesOptions() {
   var defaultOptions = {
-    ignoreCommonlyInjectedSelectors: [
-      '#intercom-stylesheet',
-      '#intercom-container',
-      '#drift-widget-container',
-      'script',
-      'head > link',
-      'head > style',
-      '#nprogress',
-      '.maps-wrapper',
-      'iframe'
-    ],
-    ignoreHtmlSelectors: [
-      '.client-card > a',
-      'svg > *'
-    ],
-    ignoreInlineStyles: {
-      '*': 'transform',
-      '.modal': 'display',
-      'h1.page-title': 'opacity'
-    },
-    ignoreAttributes: {
-      '.lazyloaded, .lazyload, .img-responsive, .lazy-load .lazy-loaded': 'src data-src data-srcset sizes width height',
-      '.client-card img': 'alt width height'
-    },
-    ignoreClassNames: {
-      '*': 'front-visible animation-ended lazyloaded lazyload lazy-load lazy-loaded nprogress-busy',
-      '.carousel-inner .item': 'active',
-      '.animate': 'animate-in',
-      '.modal': 'in',
-      '.arrow-wrapper': 'show-label'
-    },
     ignoreCommonlyInjectedElements: true,
     ignoreDynamicElements: true,
     downloadSnapshotImmediately: true
@@ -44,12 +13,7 @@ export function TrackChangesOptions() {
 
   function getAll(){
     userOptions = defaultOptions;
-    var localOptions = (JSON.parse(localStorage['userOptions'] || '{}'))
-
-    // In the future use Object.extend across each options for a better merge.
-
-    userOptions['ignoreDynamicElements'] = localOptions['ignoreDynamicElements'] || true
-    userOptions['downloadSnapshotImmediately'] = localOptions['downloadSnapshotImmediately'] || true
+    Object.assign(userOptions, (JSON.parse(localStorage['userOptions'] || '{}')));
     return userOptions;
   }
 
